4ea6e2b5929b682fa741d10f7134e35e081a074d
Arceus3251 <arceus3251@gmail.com>
2021-04-12 22:30:19 -0500

    Changed the way queries are handled
    Yeet

--- src/StudentView.java
+++ src/StudentView.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
 import java.util.Vector;
 
 public class StudentView extends JFrame {
-    static String QUERY = "";
     JTable table = new JTable();
 
     public StudentView() throws SQLException {
@@ -14,37 +13,6 @@ public class StudentView extends JFrame {
         this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         this.setTitle("Student View");
         this.setLocationRelativeTo(null);
-//        //Creating Top Bar Flair
-//        JPanel topBarPanel = new JPanel();
-//        GridLayout gl = new GridLayout(1, 5);
-//        topBarPanel.setLayout(gl);
-//        JLabel inputQuery = new JLabel("Input Query: ");
-//        JComboBox<String> comboBox = new JComboBox<>();
-//        comboBox.setEditable(false);
-//        comboBox.addItem("Sam_ID");
-//        comboBox.addItem("Name");
-//        comboBox.addItem("In_Game_Name");
-//        comboBox.addItem("Gender");
-//        comboBox.addItem("Age");
-//        comboBox.addItem("Email");
-//        JTextPane searchField = new JTextPane();
-//        JButton searchButton = new JButton("Search!");
-//        topBarPanel.add(inputQuery);
-//        topBarPanel.add(comboBox);
-//        topBarPanel.add(searchField);
-//        topBarPanel.add(searchButton);
-//        //Setting up the table environment
-//        //Creating an Actual table to display data.
-//        searchButton.addActionListener(e->{
-//            System.out.println("The Query is: "+"select "+comboBox.getSelectedItem()+" from student where = "+searchField.getText());
-//            QUERY = "select "+comboBox.getSelectedItem()+" from student where "+searchField.getText();
-//            try {
-//                table = getTable();
-//            } catch (SQLException throwables) {
-//                throwables.printStackTrace();
-//            }
-//            table.updateUI();
-//        });
 
         JPanel topBarPanel = new JPanel();
         GridLayout topLayout = new GridLayout(1, 3);
@@ -53,9 +21,8 @@ public class StudentView extends JFrame {
         JTextPane searchField = new JTextPane();
         JButton searchButton = new JButton("Search!");
         searchButton.addActionListener(e -> {
-            QUERY = searchField.getText();
             try {
-                table = getTable();
+                table = getTable(searchField.getText());
             } catch (SQLException throwables) {
 
                 throwables.printStackTrace();
@@ -65,7 +32,7 @@ public class StudentView extends JFrame {
         topBarPanel.add(searchLabel);
         topBarPanel.add(searchField);
         topBarPanel.add(searchButton);
-        table = getTable();
+        table = getTable("");
         //Adds everything to frame
         this.add(BorderLayout.NORTH, topBarPanel);
         this.add(new JScrollPane(table));
@@ -73,16 +40,12 @@ public class StudentView extends JFrame {
     }
 
     //Gathers the data to be viewed in a JTable
-    public static JTable getTable() throws SQLException {
+    public static JTable getTable(String query) throws SQLException {
         Connection conn;
         try {
             conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/shsu_gaming_db", "root", "hongvy123");
-            String query = "";
-            if (QUERY.equals("")) {
+            if(query.equals("")) {
                 query = "select * from student";
-            } else {
-                query = QUERY;
-
             }
             Statement stmt = conn.createStatement();
             ResultSet rs = stmt.executeQuery(query);
